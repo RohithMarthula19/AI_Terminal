4236f4b0254d5ab5c37f97f55086aee4fd038f00
RohithMarthula19 <rohithmarthula@gmail.com>
2026-05-12 14:21:10 +0530

    fix : Updated config

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,5 @@
-const API_URL = "https://ai-terminal-pxs0.onrender.com";
+const API_URL = window.location.hostname === "localhost"
+  ? "http://localhost:5000"  
+  : "https://ai-terminal-pxs0.onrender.com";
+
 export default API_URL;
